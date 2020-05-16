163eab52095f34b21f296ce2d6964ff7875ef3cc
Azizjon Muminov <azick7553@gmail.com>
2020-05-16 18:27:09 +0500

    Add project files.

---
+++ DependeciInvertionSOLID/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DependeciInvertionSOLID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book(new ConsolePrinter());
+            book.Print();
+        }
+    }
+
+    class Book
+    {
+        public string Text { get; set; }
+        public IPrinter Printer { get; set; }
+        public Book(IPrinter printer)
+        {
+            this.Printer = printer;
+        }
+        public void Print()
+        {
+            Printer.Print(Text);
+        }
+
+    }
+
+    interface IPrinter
+    {
+        void Print(string text);
+    }
+    class ConsolePrinter : IPrinter
+    {
+        public void Print(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
